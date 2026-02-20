c8a347a957c6ec846a9ef778c5fe4d7f513ca3d7
IndigoDarkEagle <govera4009@masseyhigh.school.nz>
2026-02-20 13:02:46 +1300

    Working instructions, partial rounds

---
+++ C_04_One_Round_v1.py
@@ -0,0 +1,20 @@
+import random
+
+# Intialise rounds points
+user_points = 0
+comp_points = 0
+double_user = "no"
+
+# Roll the dice for the user and note if they got a double
+user_one = random.randint(1, 6)
+
+
+# roll the dice for the computer
+
+
+# Update the user / computer points
+
+
+
+# Output the results
+# Let the user know if they qualify for double points
